2f7cb9c4f5a9e7ca77a489f87760135f3ec94f51
Gerardtit <gperalta83.gp@gmail.com>
2023-02-06 18:12:04 -0300

    Introducing error

--- cypress/e2e/onboarding/step1.cy.js
+++ cypress/e2e/onboarding/step1.cy.js
@@ -6,5 +6,13 @@ describe('template spec', () => {
     cy.get('#query-btn').should('contain', 'Button2')
     //explicit assertions
     expect(true).to.be.true
+  })  
+  it('C37 Test 3', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('get').click()
+    // Implicit assertion
+    cy.get('#query-btn').should('contain', 'Button')
+    //explicit assertions
+    expect(true).to.be.true
   })
 })
